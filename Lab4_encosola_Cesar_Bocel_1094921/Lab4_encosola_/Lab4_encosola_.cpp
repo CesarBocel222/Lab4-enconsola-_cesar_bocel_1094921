ccc6529215edad36d83060a2816e74cc3259a818
Cesar <cesarbocel222@gmail.com>
2022-09-30 17:47:40 -0600

    Funcion insertar

--- Lab4_encosola_Cesar_Bocel_1094921/Lab4_encosola_/Lab4_encosola_.cpp
+++ Lab4_encosola_Cesar_Bocel_1094921/Lab4_encosola_/Lab4_encosola_.cpp
@@ -13,7 +13,6 @@ typedef struct nodo* Tlista;
 void agregar(Tlista& lista, int valor)
 {
     Tlista t, q = new(struct nodo);
-
     q->nodo = valor;
     q->sig = NULL;
 
@@ -30,5 +29,50 @@ void agregar(Tlista& lista, int valor)
         }
         t->sig = q;
     }
+}
+int insertarAntesoDespues()
+{
+    int _op, band;
+    cout << endl;
+    cout << "\t 1. Antes de la posicion           " << endl;
+    cout << "\t 2. Despues de la posicion         " << endl;
 
+    cout << "\n\t Opcion : "; cin >> _op;
+
+    if (_op == 1)
+        band = -1;
+    else
+        band = 0;
+
+    return band;
+}
+void insertar(Tlista& lista, int valor, int pos)
+{
+    Tlista q, t;
+    int i;
+    q = new(struct nodo);
+    q->nodo = valor;
+
+    if (pos == 1)
+    {
+        q->sig = lista;
+        lista = q;
+    }
+    else
+    {
+        int x = insertarAntesoDespues();
+        t = lista;
+
+        for (i = 1; t != NULL; i++)
+        {
+            if (i == pos + x)
+            {
+                q->sig = t->sig;
+                t->sig = q;
+                return;
+            }
+            t = t->sig;
+        }
+    }
+    cout << "   Error...Posicion no encontrada..!" << endl;
 }
