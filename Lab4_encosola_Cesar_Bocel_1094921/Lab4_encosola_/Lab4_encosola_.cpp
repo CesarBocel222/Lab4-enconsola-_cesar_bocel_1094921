ac886609e66097890e2d22b82c286f387996c777
Cesar <cesarbocel222@gmail.com>
2022-09-30 17:42:31 -0600

    Funcion agregar

--- Lab4_encosola_Cesar_Bocel_1094921/Lab4_encosola_/Lab4_encosola_.cpp
+++ Lab4_encosola_Cesar_Bocel_1094921/Lab4_encosola_/Lab4_encosola_.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
+//creacion del nodo y de alghunas funciones basada de la pagina https://blog.martincruz.me/2012/10/listas-enlazadas-simples-en-c.html
 struct nodo {
     int nodo;        // en este caso es un numero entero
     struct nodo* sig;
@@ -10,3 +10,25 @@ struct nodo {
 
 typedef struct nodo* Tlista;
 
+void agregar(Tlista& lista, int valor)
+{
+    Tlista t, q = new(struct nodo);
+
+    q->nodo = valor;
+    q->sig = NULL;
+
+    if (lista == NULL)
+    {
+        lista = q;
+    }
+    else
+    {
+        t = lista;
+        while (t->sig != NULL)
+        {
+            t = t->sig;
+        }
+        t->sig = q;
+    }
+
+}
